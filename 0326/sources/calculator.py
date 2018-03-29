200d0f9de56f2be473dcefa24649a2a498ec4081
yenting25 <37055295+yenting25@users.noreply.github.com>
2018-03-29 12:18:00 +0800

    123
    123

---
+++ 0326/sources/calculator.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+def calculator(n1 ,n2 , func):
+    return func(n1,n2)
+def func_add(n1,n2):
+    return int(n1)+int(n2)
+
+def func_minus(n1,n2):
+    return int(n1)-int(n2)
+
+def func_cross(n1,n2):
+    return int(n1)*int(n2)
+
+def func_div(n1,n2):
+    return int(n1)/int(n2)
+
